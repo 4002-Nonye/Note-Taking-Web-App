30e32c39cf87f23d699206665523e6a5e9e82e3d
4002-Nonye <royaltysungbin@gmail.com>
2025-05-13 14:10:21 +0100

    set up prod keys

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET,
+  mongoURI: process.env.MONG0_URI,
+};
