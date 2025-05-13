0629dd3c8e5038ef2263e567e6d6414a51498ab6
4002-Nonye <royaltysungbin@gmail.com>
2025-05-13 18:16:49 +0100

    fix: react-router-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
-import App from './App.jsx';
-import Login from './pages/Login.jsx';
-import Register from './pages/Register.jsx';
-
-// import axios from 'axios';
-
-// window.axios = axios;
 const router = createBrowserRouter([
   {
     path: '/',
-    Component: App,
+    element: <App />,
     children: [
-      { index: true, Component: Login },
-      { path: '/register', Component: Register },
+      { index: true, element: <Login /> },
+      { path: 'register', element: <Register /> },
     ],
   },
 ]);
