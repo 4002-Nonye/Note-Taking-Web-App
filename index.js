30e7c17906028c5c823e9e9e30bb1e7eacbfdef8
4002-Nonye <royaltysungbin@gmail.com>
2025-05-13 18:06:34 +0100

    fix: import order

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 require('./services/passport');
+const path = require('path');
 
 mongoose.connect(keys.mongoURI);
 
@@ -22,20 +23,16 @@ require('./routes/localAuthRoutes')(app);
 
 
 // HANDLING ROUTES IN PRODUCTION
-const path = require('path');
-
-
 if (process.env.NODE_ENV === 'production') {
   // express will serve up production assets like main.js or main.css
   app.use(express.static(path.join(__dirname,'/client/dist')));
 
-  const path = require('path');
+
   app.get('*', (_, res) =>
     res.sendFile(path.join(__dirname, '/client', 'dist', 'index.html'))
   );
 }
-const data =path.join(__dirname, '/client', 'dist', 'index.html')
-console.log(data)
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT);
