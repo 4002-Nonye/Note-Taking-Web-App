ad88722518d0b97224593081233a72a59793e94f
4002-Nonye <royaltysungbin@gmail.com>
2025-05-25 23:59:13 +0100

    Add more styles

--- client/src/components/ColorTab.jsx
+++ client/src/components/ColorTab.jsx
@@ -2,26 +2,44 @@ import { IoSunny, IoMoon } from "react-icons/io5";
 
 function ColorTab() {
   return (
-    <div >
-      <h3>Color Theme</h3>
+    <div className="px-7 pt-9 text-sm">
+        <div className='mb-6'>
+        <h3 className="font-bold text-lg">Color Theme</h3>
       <p>Choose your color theme</p>
 
-      <div className="flex items-center border-[1px] border-gray-300 gap-3 p-3">
-        <IoSunny />
+        </div>
+     
+
+
+
+      <div className="flex flex-col gap-7">
+
+
+
+      <div className="flex w-2/4 items-center gap-3 rounded-md border-[1px] border-gray-300 p-3 cursor-pointer">
+        <div className="rounded-md border-[1px] border-gray-300 p-3 text-2xl">
+          <IoSunny className="text-2xl" />
+        </div>
 
         <div>
-          <p>Light Mode</p>
+          <p className="font-bold">Light Mode</p>
           <p>Pick a clean and classic light theme</p>
         </div>
       </div>
 
-      <div>
-        <IoMoon />
+      <div className="flex w-2/4 items-center gap-3 rounded-md border-[1px] border-gray-300 p-3 cursor-pointer">
+        <div className="rounded-md border-[1px] border-gray-300 p-3 text-2xl">
+          <IoMoon />
+        </div>
 
         <div>
-          <p>Dark Mode</p>
+          <p className="font-bold">Dark Mode</p>
           <p>Select a sleek and modern dark theme</p>
         </div>
+      </div>
+
+
+
       </div>
     </div>
   );
---
+++ client/src/components/FontTab.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FontTab() {
+  return (
+    <div>FontTab</div>
+  )
+}
+
+export default FontTab
