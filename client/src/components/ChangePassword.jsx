a6fd46f61aad30d130bd80eaa9a637c6d1f31610
4002-Nonye <royaltysungbin@gmail.com>
2025-06-04 23:08:57 +0100

    fix bug

--- client/src/components/ChangePassword.jsx
+++ client/src/components/ChangePassword.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { useForm } from "react-hook-form";
+import { FaArrowLeft } from "react-icons/fa6";
+
 import PasswordVisibility from "./PasswordVisibility";
 import Button from "./Button";
 import Errors from "./Errors";
 
 function ChangePassword() {
+  const navigate = useNavigate();
   const [visibility, setVisibility] = useState({
     current: false,
     new: false,
@@ -31,104 +36,116 @@ function ChangePassword() {
   };
 
   return (
-    <div className="px-7 pt-9 pb-7">
-      <h3 className="mb-6 text-lg font-bold">Change Password</h3>
-
-      <form
-        onSubmit={handleSubmit(onSubmit, onError)}
-        className="flex flex-col gap-4"
+    <>
+      <Button
+        onclick={() => navigate("/account/settings")}
+        customClass="group flex gap-2 pt-3 px-3 text-gray-500 block lg:hidden  items-center xl:hidden text-sm"
       >
-        {/* Current Password */}
-        <div className="relative w-full md:w-3/4 xl:w-2/4">
-          <label htmlFor="currentPassword" className="text-sm">
-            Old Password
-          </label>
+        <FaArrowLeft className="group-hover:text-primaryBlue text-sm transition-all duration-500" />
+        <span>Settings</span>
+      </Button>
+      <div className="px-7 pt-9 pb-7">
+        <h3 className="mb-6 text-lg font-bold">Change Password</h3>
 
-          <input
-            autoComplete="current-password"
-            type={visibility.current ? "text" : "password"}
-            id="currentPassword"
-            className="dark:border-darkBorder mt-2 w-full rounded-md border-2 border-gray-300 p-3 outline-0"
-            {...register("currentPassword", {
-              required: "Current password is required",
-            })}
-          />
-          <PasswordVisibility
-            customClass="absolute top-12 right-2"
-            passwordVisible={visibility.current}
-            setPasswordVisible={() => toggleVisibility("current")}
-          />
-          {errors.currentPassword && (
-            <Errors err={errors.currentPassword.message} />
-          )}
-        </div>
+        <form
+          onSubmit={handleSubmit(onSubmit, onError)}
+          className="flex flex-col gap-4"
+        >
+          {/* Current Password */}
+          <div className="relative w-full md:w-3/4 xl:w-2/4">
+            <label htmlFor="currentPassword" className="text-sm">
+              Old Password
+            </label>
 
-        {/* New Password */}
-        <div className="relative w-full md:w-3/4 xl:w-2/4">
-          <label htmlFor="newPassword" className="text-sm">
-            New Password
-          </label>
+            <input
+              autoComplete="current-password"
+              type={visibility.current ? "text" : "password"}
+              id="currentPassword"
+              className="dark:border-darkBorder mt-2 w-full rounded-md border-2 border-gray-300 p-3 outline-0"
+              {...register("currentPassword", {
+                required: "Current password is required",
+              })}
+            />
+            <PasswordVisibility
+              customClass="absolute top-12 right-2"
+              passwordVisible={visibility.current}
+              setPasswordVisible={() => toggleVisibility("current")}
+            />
+            {errors.currentPassword && (
+              <Errors err={errors.currentPassword.message} />
+            )}
+          </div>
 
-          <input
-          autoComplete="new-password"
-            type={visibility.new ? "text" : "password"}
-            id="newPassword"
-            className="dark:border-darkBorder mt-2 w-full rounded-md border-2 border-gray-300 p-3 outline-0"
-            {...register("newPassword", {
-              required: "New password is required",
-              minLength: {
-                value: 8,
-                message: "New password must be at least 8 characters",
-              },
-            })}
-          />
-          <PasswordVisibility
-            customClass="absolute top-12 right-2"
-            passwordVisible={visibility.new}
-            setPasswordVisible={() => toggleVisibility("new")}
-          />
+          {/* New Password */}
+          <div className="relative w-full md:w-3/4 xl:w-2/4">
+            <label htmlFor="newPassword" className="text-sm">
+              New Password
+            </label>
 
-          <p className="text-[12px] text-gray-700 italic dark:text-gray-500">
-            At least 8 characters
-          </p>
-          {errors.newPassword && <Errors err={errors.newPassword.message} />}
-        </div>
+            <input
+              autoComplete="new-password"
+              type={visibility.new ? "text" : "password"}
+              id="newPassword"
+              className="dark:border-darkBorder mt-2 w-full rounded-md border-2 border-gray-300 p-3 outline-0"
+              {...register("newPassword", {
+                required: "New password is required",
+                minLength: {
+                  value: 8,
+                  message: "New password must be at least 8 characters",
+                },
+              })}
+            />
+            <PasswordVisibility
+              customClass="absolute top-12 right-2"
+              passwordVisible={visibility.new}
+              setPasswordVisible={() => toggleVisibility("new")}
+            />
 
-        {/* Confirm Password */}
-        <div className="relative w-full md:w-3/4 xl:w-2/4">
-          <label htmlFor="confirmPassword" className="text-sm">
-            Confirm New Password
-          </label>
+            <p className="text-[12px] text-gray-700 italic dark:text-gray-500">
+              At least 8 characters
+            </p>
+            {errors.newPassword && <Errors err={errors.newPassword.message} />}
+          </div>
 
-          <input
-           autoComplete="new-password"
-            type={visibility.confirm ? "text" : "password"}
-            id="confirmPassword"
-            className="dark:border-darkBorder mt-2 w-full rounded-md border-2 border-gray-300 p-3 outline-0"
-            {...register("confirmPassword", {
-              required: "Please confirm your password",
-              validate: (value) =>
-                value === getValues("newPassword") || "Passwords do not match",
-            })}
-          />
-          <PasswordVisibility
-            customClass="absolute top-12 right-2"
-            passwordVisible={visibility.confirm}
-            setPasswordVisible={() => toggleVisibility("confirm")}
-          />
-          {errors.confirmPassword && <Errors err={errors.confirmPassword.message} />}
-        </div>
+          {/* Confirm Password */}
+          <div className="relative w-full md:w-3/4 xl:w-2/4">
+            <label htmlFor="confirmPassword" className="text-sm">
+              Confirm New Password
+            </label>
 
-        <div className="relative flex w-full justify-end md:w-3/4 xl:w-2/4">
-          <Button
-            type="submit"
-            customClass="bg-primaryBlue text-white rounded-md justify-center md:w-[35%] w-2/4"
-          >
-            Save Password
-          </Button>
-        </div>
-      </form>
-    </div>
+            <input
+              autoComplete="new-password"
+              type={visibility.confirm ? "text" : "password"}
+              id="confirmPassword"
+              className="dark:border-darkBorder mt-2 w-full rounded-md border-2 border-gray-300 p-3 outline-0"
+              {...register("confirmPassword", {
+                required: "Please confirm your password",
+                validate: (value) =>
+                  value === getValues("newPassword") ||
+                  "Passwords do not match",
+              })}
+            />
+            <PasswordVisibility
+              customClass="absolute top-12 right-2"
+              passwordVisible={visibility.confirm}
+              setPasswordVisible={() => toggleVisibility("confirm")}
+            />
+            {errors.confirmPassword && (
+              <Errors err={errors.confirmPassword.message} />
+            )}
+          </div>
+
+          <div className="relative flex w-full justify-end md:w-3/4 xl:w-2/4">
+            <Button
+              type="submit"
+              customClass="bg-primaryBlue text-white rounded-md justify-center md:w-[35%] w-2/4"
+            >
+              Save Password
+            </Button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 
